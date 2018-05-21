da5e55d0a3b81eb434c210c99112c7630fc939aa
XuYue <xuyue@richinfo.cn>
2018-05-21 19:26:50 +0800

    ''

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+
 module.exports = {
     title: 'My Heart, My Words',
     description: '心之所想，笔之所往',
@@ -7,32 +8,41 @@ module.exports = {
     editLinks: true,
     editLinkText: '帮助我们改进页面内容！',
     themeConfig: {
-      nav: [
-        { text: '前端三剑客', link: '/front/' },
-        { text: '工具相关', link: '/tools-about/' },
-        { text: 'Github', link: 'https://github.com/Happy-Everyday/free-write/tree/master/docs' }
-      ],
-      sidebar: {
-        '/front/': [{
-          title: '前端三剑客',
-          collapsable: false,
-          children: [
-            ['', '介绍'],
-            ['HTML.md', 'HTML'],
-            ['CSS.md', 'CSS'],
-            ['JS.md', 'JS']  
-          ]
-        }],
-        '/tools-about/': [{
-          title: '工具相关',
-          collapsable: false,
-          children: [
-            ['', '介绍'],
-            ['git.md', 'GIT'],
-            ['idea.md', '编辑器'],
-            ['tools-websites.md', '工具网站']
-          ]
-        }]
-      }
+        nav: [
+            { text: '前端三剑客', link: '/front/' },
+            { text: '服务器相关', link: '/server-operation/' },
+            { text: '工具相关', link: '/tools-about/' },
+            { text: 'Github', link: 'https://github.com/Happy-Everyday/free-write/tree/master/docs' }
+        ],
+        sidebar: {
+            '/front/': [{
+                title: '前端三剑客',
+                collapsable: false,
+                children: [
+                    ['', '介绍'],
+                    ['HTML.md', 'HTML'],
+                    ['CSS.md', 'CSS'],
+                    ['JS.md', 'JS']
+                ]
+            }],
+            '/tools-about/': [{
+                title: '工具相关',
+                collapsable: false,
+                children: [
+                    ['', '介绍'],
+                    ['git.md', 'GIT'],
+                    ['idea.md', '编辑器'],
+                    ['tools-websites.md', '工具网站']
+                ]
+            }],
+            '/server-operation/': [{
+                title: '服务器相关',
+                collapsable: false,
+                children: [
+                    ['', '介绍'],
+                    ['vsftp.md', 'vsftp安装、配置、卸载']
+                ]
+            }]
+        }
     }
-  }
+}
